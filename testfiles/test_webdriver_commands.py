958fdd935f835dc8c6e0cf4001bdd2a19f8a732a
Ralph Heinkel <rh@ralph-heinkel.com>
2012-02-29 18:33:01 +0100

    - lots of documentation - cleaner setup of decorated wrapper functions - more unit tests

--- testfiles/test_webdriver_commands.py
+++ testfiles/test_webdriver_commands.py
@@ -1,7 +1,7 @@
 """
 UT module to test webdriver commands
 """
-import sys
+import sys, pytest
 sys.path.insert(0, '..')
 ###
 from selenium import webdriver
@@ -23,7 +23,40 @@ class Test_WebDriverCommands(object):
     def teardown_method(self, method):
         self.driver.quit()
 
-    def test_verifyText(self):
+    def test_all_Text_methods(self):
+        """check getText / verifyText / assertText / storeText / ... methods """
         self.wdc('open', '/static/page1')
+        #
         assert self.wdc('getText', 'css=h1') == 'H1 text'
+        #
         self.wdc('verifyText', 'css=h1', 'H1 text')
+        assert self.wdc.getVerificationErrors() == []
+        #
+        # check failing verfiyText command:
+        self.wdc('verifyText', 'css=h1', 'H1 WROOOOOONG text')
+        assert self.wdc.getVerificationErrors() == ['Value "H1 text" did not match "H1 WROOOOOONG text"']
+        self.wdc.initVerificationErrors()  # reset verification messages
+        #
+        self.wdc('assertText', 'css=h1', 'H1 text')
+        #
+        # check that wrong text raises AssertionError
+        with pytest.raises(AssertionError):
+            self.wdc('assertText', 'css=h1', 'H1 WROOOOOONG text')
+        #
+        # check that correct text raises AssertionError for 'assertNotText'
+        with pytest.raises(AssertionError):
+            self.wdc('assertNotText', 'css=h1', 'H1 text')
+        #
+        self.wdc('storeText', 'css=h1', 'h1-content')
+        assert self.wdc.storedVariables['h1-content'] == 'H1 text'
+        #
+        self.wdc('waitForText', 'css=h1', 'H1 text')
+        #
+        # Check that waiting for non-existing text finally raises RuntimeError (after timeout):
+        with pytest.raises(RuntimeError):
+            self.wdc('waitForText', 'css=h1', 'H1 WROOOOOONG text', timeout=1)
+        #
+        # check that waiting for existing text with 'waitForNotText' raises RuntimeError (after timeout)
+        with pytest.raises(RuntimeError):
+            self.wdc('waitForNotText', 'css=h1', 'H1 text', timeout=1)
+
